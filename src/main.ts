e5d9cfa170e0b4be159284b6811a2ea2b8329ba8
argon1025 <argon1025@gmail.com>
2021-06-04 14:06:21 +0900

    Add HotLoad

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.port || 3000;
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  console.log(`Server is now http://localhost:${port}`);
 }
 bootstrap();
